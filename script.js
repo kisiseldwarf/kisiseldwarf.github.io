9b88707ae5dd4b8ef6d87989489298c6784f5ba2
Hadi Kaïs <kaiskhan.hadi@enseirb-matmeca.fr>
2019-11-05 13:42:34 +0100

    Putting my work in

---
+++ script.js
@@ -0,0 +1,7 @@
+function scrolling(){
+  var div_id = "work"
+  var body = document.getElementById("the_body")
+  var html = document.getElementById("the_html")
+  var div = document.getElementById(div_id)
+  div.scrollIntoView()
+}
